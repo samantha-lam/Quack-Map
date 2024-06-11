6dd195529222e2d9d897e227ace49c761f1003b6
Samantha Lam <samlam2316@gmail.com>
2024-06-11 09:46:47 -0500

    Initial Commit

---
+++ Quack Map/Quack_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quack_MapApp.swift
+//  Quack Map
+//
+//  Created by Scholar on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Quack_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
